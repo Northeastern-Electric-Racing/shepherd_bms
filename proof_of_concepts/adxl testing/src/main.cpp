130cf6b6e333e0527297209ed850b8a021b10edf
Nick <nwdepatie@gmail.com>
2022-03-02 22:12:15 -0500

    Consolidated ADXL code into functions

--- proof_of_concepts/adxl testing/src/main.cpp
+++ proof_of_concepts/adxl testing/src/main.cpp
@@ -9,62 +9,130 @@
 #define ADXL312_XYZDATA_REG_OFFSET      0x32 
 
 
+bool verifyFunctionality();
+void configureForMeasurement();
+void getXYZ(uint8_t *msg);
+
+
 void setup() {
   Serial.begin(9600);
   Wire.begin();
   Serial.print("Initializing...");
-  Wire.beginTransmission(ADXL312_I2C_ADDR);
-  Wire.write(ADXL312_POWER_CTRL_REG);
-  Wire.write(ADXL312_POWER_CTRL_MEASURECMD);
-  Wire.endTransmission(false);
+  configureForMeasurement();
 }
 
 void loop() {
-  uint8_t idmsg[1]={0};
-  Wire.beginTransmission(ADXL312_I2C_ADDR);
-  Wire.write(ADXL312_DEVID_REG);
-  Wire.endTransmission(false);
-  Wire.requestFrom(ADXL312_I2C_ADDR, 1);
-
-  if (Wire.available())
+  if(verifyFunctionality())
   {
-      uint8_t i2cByte=0;
-
-      while(Wire.available())
-      {
-         idmsg[i2cByte] = Wire.read();
-      }
+    Serial.println("ahhh");
   }
-
-  Serial.print("Device ID:\t");
-  Serial.println(idmsg[0],HEX);
 
   uint8_t *msg = new uint8_t[6];
-
-  Wire.beginTransmission(ADXL312_I2C_ADDR);
-  Wire.write(ADXL312_XYZDATA_REG_OFFSET);
-  Wire.endTransmission(false);
-  Wire.requestFrom(ADXL312_I2C_ADDR, 6);
-
-  if (Wire.available())
-  {
-      uint8_t i2cByte=0;
-
-      while(Wire.available())
-      {
-          msg[i2cByte] = Wire.read();
-          i2cByte++;
-      }
-  }
-
-  Serial.print("XYZ Data:\t");
   int16_t XYZDATA;
+
+  getXYZ(msg);
   for(int i=0;i<6;i+=2)
   {
     XYZDATA = (msg[i]) | (msg[i+1] << 8);
     Serial.println(XYZDATA,DEC);
   }
-
+  
   Serial.println("cycle...");
-  delay(5);
+
+  delay(500);
+}
+
+/**
+ * @brief Requesting data to read in from the ADXL312
+ * 
+ * @param msg 
+ * @param numBytes 
+ * @return true 
+ * @return false 
+ */
+bool ADXL312read(uint8_t *msg, uint8_t numBytes)
+{
+    Wire.requestFrom(ADXL312_I2C_ADDR, (int)numBytes);
+
+    if (Wire.available())
+    {
+        uint8_t i2cByte=0;
+        while(Wire.available())
+        {
+            msg[i2cByte] = Wire.read();
+            i2cByte++;
+        }
+        return true;
+    }
+
+    return false;
+}
+
+/**
+ * @brief Writing to the local I2C bus with the address of the ADXL312
+ * 
+ * @param msg 
+ * @param numBytes
+ */
+void ADXL312write(uint8_t *msg, uint8_t numBytes)
+{
+    Wire.beginTransmission(ADXL312_I2C_ADDR);
+    for(uint8_t i=0; i<numBytes;i++)
+    {
+        Wire.write(msg[i]);
+    }
+    Wire.endTransmission(false);
+}
+
+
+/**
+ * @brief Verify the functionality of the onboard ADXL312
+ * 
+ * @return true 
+ * @return false 
+ */
+bool verifyFunctionality()
+{
+    uint8_t cmd[1] = {ADXL312_DEVID_REG}; 
+    uint8_t msg[1];
+    ADXL312write(cmd, 1);
+    if(ADXL312read(msg, 1))
+    {
+        Serial.println(msg[0], HEX);
+        if(msg[0] == ADXL312_DEVID)
+        {
+            Serial.println("ADXL312 connection online...");
+            return true;
+        }
+    }
+    Serial.println("ERROR: Connection to ADXL312 is not available!");
+    return false;
+}
+
+/**
+ * @brief Setting the ADXL312 to actively measure the XYZ data
+ * 
+ */
+void configureForMeasurement()
+{
+    uint8_t cmd[2] = {ADXL312_POWER_CTRL_REG, ADXL312_POWER_CTRL_MEASURECMD}; 
+    ADXL312write(cmd, 1);
+}
+
+/**
+ * @brief Retrieves the XYZ data from the ADXL312
+ * 
+ * @return uint8_t* msg
+ */
+void getXYZ(uint8_t *msg)
+{
+    uint8_t cmd[1] = {ADXL312_XYZDATA_REG_OFFSET}; 
+    ADXL312write(cmd, 1);
+    if(ADXL312read(msg, 6))
+    {
+        return;
+    }
+    Serial.println("ERROR: XYZ Data not available!");
+    return;
+    
 }
