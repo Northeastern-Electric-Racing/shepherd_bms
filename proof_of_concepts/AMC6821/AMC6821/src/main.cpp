f9e4cc607823a0b05d7d0829561ff8436ce7bd37
Nick <nwdepatie@gmail.com>
2022-03-06 00:02:59 -0500

    Figured out flow of writing PWM duty cycle and created relevant macros

--- proof_of_concepts/AMC6821/AMC6821/src/main.cpp
+++ proof_of_concepts/AMC6821/AMC6821/src/main.cpp
@@ -1,5 +1,57 @@
+/**
+ * https://www.ti.com/lit/ds/symlink/amc6821.pdf?ts=1644706226375&ref_url=https%253A%252F%252Fwww.ti.com%252Fproduct%252FAMC6821%253Futm_source%253Dgoogle%2526utm_medium%253Dcpc%2526utm_campaign%253Dasc-sens-null-prodfolderdynamic-cpc-pf-google-wwe%2526utm_content%253Dprodfolddynamic%2526ds_k%253DDYNAMIC%2BSEARCH%2BADS%2526DCM%253Dyes%2526gclid%253DCj0KCQiA0p2QBhDvARIsAACSOOPKQVP7tfyxbaC8997ZjeHcQWZiSwAi1yblV-rFrJZ4BQS3xCwo1iYaAjmLEALw_wcB%2526gclsrc%253Daw.ds
+ * ^Datasheet
+ * 
+ * @note Look at pages 16 and 17 for PWM operation
+ */
+
 #include <Arduino.h>
 #include <Wire.h> //I2C library 
+
+
+#define AMC6821_I2C_ADDR                    0x18
+#define AMC6821_DEVID_REG                   0x3D
+#define AMC6821_DEVID                       0x21
+
+
+/**
+ * @note Duty Cycle is variable corresponding to 1/255 increments 
+ *       (0x00 corresponds to 0% duty cycle and 0xFF corresponds to 100% duty cycle)
+ * @ref Refer to page 40 of datasheet
+ */
+#define AMC6821_DUTYCYCLE_REG               0x22
+
+
+/**
+ * @brief PWM I2C Characteristics Command Contents  (8 bits)
+ * @ref Refer to page 41 of datasheet
+ *                _______________________________________________________________________________________________________________
+ * bit numbers:   |            7            |      6      |           5   4   3               |    2   1   0                    |
+ *                |-------------------------|-------------|-----------------------------------|---------------------------------|
+ * contents:      |   Fan spin up disable   |   Reserved  |   PWM Frequency(see AMC macros)   |   Spin up time(see AMC macros)  |
+ *                |_________________________|_____________|___________________________________|_________________________________|
+ * 
+ */
+#define AMC6821_CHARACTERISTICS_REG         0x20
+//PWM Frequencies
+#define AMC6821_CHARACTERISTICS_1KHZ        0x00
+#define AMC6821_CHARACTERISTICS_10KHZ       0x01
+#define AMC6821_CHARACTERISTICS_20KHZ       0x02
+#define AMC6821_CHARACTERISTICS_25KHZ       0x03
+#define AMC6821_CHARACTERISTICS_30KHZ       0x04
+#define AMC6821_CHARACTERISTICS_40KHZ       0x05    //0x06 and 0x07 also result in 40kHz duty cycle
+//Fan spin up times
+#define AMC6821_CHARACTERISTICS_SPINUP_02   0x00    //02 means 0.2 seconds
+#define AMC6821_CHARACTERISTICS_SPINUP_04   0x01    //04 means 0.4 seconds
+#define AMC6821_CHARACTERISTICS_SPINUP_06   0x02    //...
+#define AMC6821_CHARACTERISTICS_SPINUP_08   0x03
+#define AMC6821_CHARACTERISTICS_SPINUP_1    0x04
+#define AMC6821_CHARACTERISTICS_SPINUP_2    0x05
+#define AMC6821_CHARACTERISTICS_SPINUP_4    0x06
+#define AMC6821_CHARACTERISTICS_SPINUP_8    0x07
+
+
+/******************************************************************************************************************/
 
 void setup() {
   // put your setup code here, to run once:
@@ -13,10 +65,10 @@ void loop() {
   int idmsg[1];
 
  
-  Wire.beginTransmission(0x18);
-  Wire.write(0x3D);
+  Wire.beginTransmission(AMC6821_I2C_ADDR);
+  Wire.write(AMC6821_DEVID_REG);
   Wire.endTransmission(false);
-  Wire.requestFrom(0x18, 1);
+  Wire.requestFrom(AMC6821_I2C_ADDR, 1);
 
   if (Wire.available())
   {
