3c1add9842f2c7a1b51e992b4d5c4b46db5c68d4
wfox5812 <willffox9183@gmail.com>
2022-10-05 20:11:17 -0400

    created accumulator data type

--- shepherd_bms/include/datastructs.h
+++ shepherd_bms/include/datastructs.h
@@ -1,5 +1,35 @@
 #ifndef DATASTRUCTS_H
 #define DATASTRUCTS_H
 
+#include <nerduino.h>
+
+/*individual segment data*/
+typedef struct ChipData_t
+{
+double VoltageReading[12];          //store voltage readings from each chip
+double thermistorReading[32];       //store all therm readings from each chip
+bool discharge[12];
+};
+
+/*One frame of data from the accumulator*/
+struct AccumulatorData_t
+{
+    ChipData_t ChipData[8];
+
+    /*max and min thermistor readings*/
+    double maxTemp;
+    double minTemp;
+
+    /*max, min, and avg voltage of the cells*/
+    double maxVoltage;
+    double minVoltage;
+    double avgVoltage;
+
+    FaultStatus_t faulted = NOT_FAULTED;
+
+
+    double currentReading;
+};
+
 
 #endif
