9af2722a383566d8c29d8052bfe05bb28360d030
Nick <nwdepatie@gmail.com>
2022-10-22 20:47:36 -0400

    Enumerated fault codes and created a structure for storing a critical value of a specific cell

--- shepherd_bms/include/datastructs.h
+++ shepherd_bms/include/datastructs.h
@@ -3,9 +3,10 @@
 
 #include <nerduino.h>
 
-
-
-/*Individual chip data*/
+/**
+ * @brief Individual chip data
+ * @note stores thermistor values, voltage readings, and the discharge status
+ */
 typedef struct ChipData_t
 {
     uint16_t VoltageReading[12];          //store voltage readings from each chip
@@ -13,35 +14,57 @@ typedef struct ChipData_t
     bool discharge[12];
 };
 
-typedef enum BMSFault_t
+/**
+ * @brief Enuemrated possible fault codes for the BMS
+ * @note  the values increase at powers of two to perform bitwise operations on a main fault code
+ *          to set or get the error codes
+ */
+enum BMSFault_t
 {
     //Orion BMS faults
-    CELLS_NOT_BALANCING;
-    CELL_VOLTAGE_TOO_HIGH;
-    CELL_VOLTAGE_TOO_LOW;
-    CELL_ASIC_FAULT;                    //internal fault with the cell voltage measurement circuitry
-    PACK_TOO_HOT;
-    CHARGE_INTERLOCK_FAULT;             //both Charge Power (pin 3) and Ready Power (pin 2) are energized at the same time
-    OPEN_WIRING_FAULT;                  //cell tap wire is either weakly connected or not connected 
-    PACK_VOLTAGE_MISMATCH;              //total pack voltage sensor did not match the sum of the individual cell voltage measurements
-    INTERNAL_SOFTWARE_FAULT;
-    INTERNAL_HEATSINK_THERMISTOR;       //internal hardware fault has occurredwith the internal thermistors that monitor the unit temperature
-    INTERNAL_HARDWARE_FAULT;
-    CELL_BALANCING_STUCK_OFF;
-    CURRENT_SENSOR_FAULT;
-    INTERNAL_CELL_COMM_FAULT;
-    LOW_CELL_VOLTAGE;                   //voltage of a cell falls below 90 mV
-    HIGH_VOLTAGE_ISOLATION_FAULT;       //isolation breakdown between the high voltage battery and the BMS low voltage power ground
-    PACK_VOLTAGE_SENSOR_FAULT;          //fault code is set if the total pack voltage sensor reads zero volts
-    WEAK_PACK_FAULT;
-    FAN_MONITOR_FAULT;
-    EXTERNAL_COMMUNICATION_FAULT;
-    DISCHARGE_LIMIT_ENFORCEMENT_FAULT;
-    CHARGER_SAFETY_RELAY;
-    BATTERY_THERMISTOR;
+    CELLS_NOT_BALANCING                 = 0x1,
+    CELL_VOLTAGE_TOO_HIGH               = 0x2,
+    CELL_VOLTAGE_TOO_LOW                = 0x4,
+    CELL_ASIC_FAULT                     = 0x8,      //internal fault with the cell voltage measurement circuitry
+    PACK_TOO_HOT                        = 0x10,
+    CHARGE_INTERLOCK_FAULT              = 0x20,     //both Charge Power (pin 3) and Ready Power (pin 2) are energized at the same time
+    OPEN_WIRING_FAULT                   = 0x40,     //cell tap wire is either weakly connected or not connected 
+    PACK_VOLTAGE_MISMATCH               = 0x80,     //total pack voltage sensor did not match the sum of the individual cell voltage measurements
+    INTERNAL_SOFTWARE_FAULT             = 0x100,
+    INTERNAL_HEATSINK_THERMISTOR        = 0x200,    //internal hardware fault has occurredwith the internal thermistors that monitor the unit temperature
+    INTERNAL_HARDWARE_FAULT             = 0x400,    
+    CELL_BALANCING_STUCK_OFF            = 0x800,
+    CURRENT_SENSOR_FAULT                = 0x1000,
+    INTERNAL_CELL_COMM_FAULT            = 0x2000,
+    LOW_CELL_VOLTAGE                    = 0x4000,   //voltage of a cell falls below 90 mV
+    HIGH_VOLTAGE_ISOLATION_FAULT        = 0x8000,   //isolation breakdown between the high voltage battery and the BMS low voltage power ground
+    PACK_VOLTAGE_SENSOR_FAULT           = 0x10000,  //fault code is set if the total pack voltage sensor reads zero volts
+    WEAK_PACK_FAULT                     = 0x20000,
+    FAN_MONITOR_FAULT                   = 0x40000,
+    EXTERNAL_COMMUNICATION_FAULT        = 0x80000,
+    DISCHARGE_LIMIT_ENFORCEMENT_FAULT   = 0x100000,
+    CHARGER_SAFETY_RELAY                = 0x200000,
+    BATTERY_THERMISTOR                  = 0x400000,
+
+    MAX_FAULTS                          = 0x80000000 //Maximum allowable fault code
 };
 
-/*One frame of data from the accumulator*/
+
+/**
+ * @brief Stores critical values for the pack, and where that critical value can be found
+ * 
+ */
+struct CrticalCellValue_t
+{
+    uint16_t val;
+    uint8_t chipIndex;
+    uint8_t cellNum;
+};
+
+/**
+ * @brief Represents one "frame" of BMS data
+ * 
+ */
 struct AccumulatorData_t
 {
     /*Array of data from all chips in the system*/
@@ -51,15 +74,19 @@ struct AccumulatorData_t
 
     uint16_t currentReading;
 
-    BMSFault_t;
+    /**
+     * @brief Note that this is a 32 bit integer, so there are 32 max possible fault codes
+     * 
+     */
+    uint32_t faultCode;
 
     /*Max and min thermistor readings*/
-    uint16_t maxTemp;
-    uint16_t minTemp;
+    CrticalCellValue_t maxTemp;
+    CrticalCellValue_t minTemp;
 
     /*Max, min, and avg voltage of the cells*/
-    uint16_t maxVoltage;
-    uint16_t minVoltage;
+    CrticalCellValue_t maxVoltage;
+    CrticalCellValue_t minVoltage;
     uint16_t avgVoltage;
 };
 
