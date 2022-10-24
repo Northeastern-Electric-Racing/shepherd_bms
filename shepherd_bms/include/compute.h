9193028d34f7c555d436f12eef6abedd644381ec
Daniel-Devory <114166910+Daniel-Devory@users.noreply.github.com>
2022-10-23 23:17:51 -0400

    Compute Interface Definition (#38)
    * Created class defintion for compute board interface

--- shepherd_bms/include/compute.h
+++ shepherd_bms/include/compute.h
@@ -2,5 +2,59 @@
 #define COMPUTE_H
 
 #include "datastructs.h"
+#include "nerduino.h"
 
+using namespace std;
+
+class ComputeInterface
+{
+    private:
+        uint8_t fanSpeed;
+
+        /**
+         * @todo These might need to be changed depending on the charging ticket
+         */
+        const uint8_t startChargingMsg[];
+        const uint8_t endChargingMsg[];
+
+        void startChargerComms();
+
+        void endChargerComms();
+
+    public:
+        ComputeInterface();
+
+        ~ComputeInterface();
+
+        /**
+         * @brief Attempts to enable/disable the charger via a second CAN line, and returns a fault if the charger is not responding
+         *
+         * @param isEnabled
+         * @return FaultStatus_t
+         */
+        FaultStatus_t enableCharging(bool isEnabled);
+
+        /**
+         * @brief Returns if we are detecting a charging voltage
+         *
+         * @return true
+         * @return false
+         */
+        bool isCharging();
+
+        /**
+         * @brief Sets the desired fan speed
+         *
+         * @param newFanSpeed
+         */
+        void setFanSpeed(uint8_t newFanSpeed);
+
+        /**
+         * @brief Returns the pack current sensor reading
+         *
+         * @return int16_t
+         */
+        int16_t getPackCurrent();
+
+};
 #endif
