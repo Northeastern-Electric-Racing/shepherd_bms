dd01a206c6d82373cee9b0de02ad63130f019508
Nick <nwdepatie@gmail.com>
2022-11-06 16:00:24 -0500

    Adding a time delay to the current voltage reading such that we are able actually read data from the chips

--- shepherd_bms/src/main.cpp
+++ shepherd_bms/src/main.cpp
@@ -26,9 +26,19 @@ void SetChipConfigurations(uint8_t localConfig[][6])
   LTC6804_wrcfg(NUM_CHIPS, localConfig);
 }
 
+void ConfigureDischarge(uint8_t chip, uint16_t cells)
+{
+  chipConfigurations[chip][4] = uint8_t(cells & 0x00FF);
+  chipConfigurations[chip][5] = (chipConfigurations[chip][5] & 0xF0) + uint8_t(cells >> 8);
+}
+
 void setup()
 {
   NERduino.begin();
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  delay(3000); // Allow time to connect and see boot up info
+  Serial.println("Hello World!");
   LTC6804_initialize();
 
   // Turn OFF GPIO 1 & 2 pull downs
@@ -36,10 +46,9 @@ void setup()
   for (int c = 0; c < NUM_CHIPS; c++)
   {
     chipConfigurations[c][0] |= 0x18;
+    ConfigureDischarge(c, 0);
   }
   SetChipConfigurations(chipConfigurations);
-
-  GetChipConfigurations(chipConfigurations);
 
   Serial.print("Chip CFG:\n");
   for (int c = 0; c < NUM_CHIPS; c++)
@@ -51,27 +60,34 @@ void setup()
     }
     Serial.println();
   }
-  Serial.println("Done");
-  delay(3000);
+  Serial.println();
 }
+
+int lastVoltTime = 0;
 
 void loop()
 {
-    // Run ADC on cell taps
-    LTC6804_adcv(); //this needs to be done before pulling from registers
+    int currTime = millis();
 
-    // Pull and print the cell voltages from registers
-    LTC6804_rdcv(0, NUM_CHIPS, rawCellVoltages);
+    // MEASURE VOLTAGES
+    if (lastVoltTime + 200 < currTime) {
+      // Run ADC on cell taps
+      LTC6804_adcv(); //this needs to be done before pulling from registers
 
-    Serial.print("Voltage:\n");
-    for (int chip = 0; chip < NUM_CHIPS; chip++)
-    {
-      for (int cell=0; cell < NUM_CELLS_PER_CHIP; cell++)
+      // Pull and print the cell voltages from registers
+      LTC6804_rdcv(0, NUM_CHIPS, rawCellVoltages);
+
+      lastVoltTime = currTime;
+
+      Serial.print("Voltage:\n");
+      for (int chip = 0; chip < NUM_CHIPS; chip++)
       {
-            Serial.print(rawCellVoltages[chip][cell]);
-            Serial.print("\t");
+        for (int cell=0; cell < NUM_CELLS_PER_CHIP; cell++)
+        {
+              Serial.print(rawCellVoltages[chip][cell]);
+              Serial.print("\t");
+        }
+      Serial.println(); //newline
       }
-		Serial.println(); //newline
-	}
-  delay(1000);
+    }
 }
