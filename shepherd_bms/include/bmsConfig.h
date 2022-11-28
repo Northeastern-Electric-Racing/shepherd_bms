802cf5cbaec0c72fbc97d8ee13621182356e4a77
Nicholas DePatie <80368116+nwdepatie@users.noreply.github.com>
2022-11-28 12:52:55 -0500

    Changing default number of segments to 1

--- shepherd_bms/include/bmsConfig.h
+++ shepherd_bms/include/bmsConfig.h
@@ -2,7 +2,7 @@
 #define BMS_CONFIG_H
 
 #define NUM_SEGMENTS        1
-#define NUM_CHIPS           NUM_SEGMENTS*2
+#define NUM_CHIPS           1//NUM_SEGMENTS*2
 #define NUM_CELLS_PER_CHIP  9
 #define NUM_THERMS_PER_CHIP 32
 
