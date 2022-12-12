d5eafbd777b50768ef1e87677b8b5b299456ad13
mccauley.ma <mccauley.ma@husky.neu.edu>
2022-12-12 13:58:00 -0500

    Updated for full pack use
    Less therms, more seggs

--- shepherd_bms/src/main.cpp
+++ shepherd_bms/src/main.cpp
@@ -37,7 +37,7 @@ void loop()
 
 	  	Serial.println(); //newline
 
-		for (int therm=0; therm < NUM_THERMS_PER_CHIP; therm++)
+		for (int therm=17; therm < 28; therm++)
 		{
 			Serial.print(testData[chip].thermistorReading[therm]);
 			Serial.print("\t");
