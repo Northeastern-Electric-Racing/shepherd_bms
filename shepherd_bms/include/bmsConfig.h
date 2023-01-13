22757068d5cac583a1d26fcd34ac80920d9f01fe
mccauley.ma <mccauley.ma@husky.neu.edu>
2023-01-13 18:01:30 -0500

    Settings clean up

--- shepherd_bms/include/bmsConfig.h
+++ shepherd_bms/include/bmsConfig.h
@@ -1,29 +1,29 @@
 #ifndef BMS_CONFIG_H
 #define BMS_CONFIG_H
 
+// Hardware definition
 #define NUM_SEGMENTS        4
 #define NUM_CHIPS           NUM_SEGMENTS*2
 #define NUM_CELLS_PER_CHIP  9
 #define NUM_THERMS_PER_CHIP 32
 
-// Hardware limits
+// Firmware limits
 #define MAX_TEMP            65 //degrees C
 #define MIN_TEMP            -15 // deg C
+#define MAX_VOLT_MEAS       65535
+#define MIN_VOLT_MEAS       0
 
 //cell limits
 #define MIN_VOLT            2.9
 #define MAX_VOLT            4.2
-#define MAX_CHARGE_VOLT     4.22
+#define MAX_CHARGE_VOLT     4.23
 #define MAX_DELTA_V         0.02
 #define BAL_MIN_V           4.00
 #define MAX_CELL_TEMP       55
-#define CHARGE_TIMEOUT      600000 // 10 minutes, may need adjustment
-
-#define MAX_VOLT_MEAS       65535
-#define MIN_VOLT_MEAS       0
-
-#define VOLT_SAG_MARGIN     0.25 // Volts above the minimum cell voltage we would like to aim for
-
 #define MAX_CELL_CURR       700 // Amps per BMS cell
+
+// Code settings
+#define CHARGE_TIMEOUT      300000 // 5 minutes, may need adjustment
+#define VOLT_SAG_MARGIN     0.25 // Volts above the minimum cell voltage we would like to aim for
 
 #endif
