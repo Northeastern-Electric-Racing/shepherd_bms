a563dba741a79f5a28f85e2fa01b926d07992ef2
nwdepatie <nwdepatie@gmail.com>
2023-01-12 22:09:20 -0500

    Car can charge now

--- shepherd_bms/src/main.cpp
+++ shepherd_bms/src/main.cpp
@@ -242,8 +242,8 @@ void shepherdMain()
 		if (currTime > lastChargeMsg + 150) {
 			lastChargeMsg = currTime;
 			compute.sendChargingMessage(packChargeVolt, accData->chargeLimit);
-			compute.sendChargingStatus(true);
 		}
+		compute.sendChargingStatus(true);
 	} else if (bmsFault == FAULTS_CLEAR) {
 		digitalWrite(CHARGE_SAFETY_RELAY, LOW);
 	}
