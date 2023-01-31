60123f5bd9a983236ea7ddde0c015167ab3233af
dyldonahue <donahue.dyl@northeastern.edu>
2023-01-31 09:38:29 -0500

    added comment to Accum datatype to show that this value is x10

--- shepherd_bms/src/compute.cpp
+++ shepherd_bms/src/compute.cpp
@@ -98,7 +98,7 @@ int16_t ComputeInterface::getPackCurrent()
 
 
     uint16_t highCurrent = (5 / current_supplyVoltage) * (analogRead(CURRENT_SENSOR_PIN_H) * current_ADCResolution - current_highChannelOffset) * highChannelGain; // Channel has a large range with low resolution
-    uint16_t lowCurrent = 10 * (5 / current_supplyVoltage) * (analogRead(CURRENT_SENSOR_PIN_L) * current_ADCResolution - current_lowChannelOffset) * lowChannelGain; // Channel has a small range with high resolution
+    int16_t lowCurrent = 10 * (5 / current_supplyVoltage) * (analogRead(CURRENT_SENSOR_PIN_L) * current_ADCResolution - current_lowChannelOffset) * lowChannelGain; // Channel has a small range with high resolution
 
     // If the current is scoped within the range of the low channel, use the low channel
     if(lowCurrent < current_lowChannelMax - 5.0 || lowCurrent > current_lowChannelMin + 5.0)
