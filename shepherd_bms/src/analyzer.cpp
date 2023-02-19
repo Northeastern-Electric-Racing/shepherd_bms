99e53bda74f04b7ad8aa80cfa02e17a8adeb6397
Hamza Iqbal <92900102+HamzaIqbal69@users.noreply.github.com>
2023-02-18 19:14:23 -0500

    "Added Fault Status Message with changes that matt requested" (#147)
    * "Added Fault Status Message with changes that matt requested"
    
    * removed static from union
    
    ---------
    
    Co-authored-by: dyldonahue <donahue.dyl@northeastern.edu>
    
    fixed messed up files

--- shepherd_bms/src/analyzer.cpp
+++ shepherd_bms/src/analyzer.cpp
@@ -23,6 +23,7 @@ void Analyzer::push(AccumulatorData_t *data)
 	calcCellResistances();
 	calcDCL();
 	calcContDCL();
+    calcCCL();
 	calcContCCL();
 	calcStateOfCharge();
 
@@ -174,6 +175,34 @@ void Analyzer::calcContDCL()
     else
     {
         bmsdata->contDCL = TEMP_TO_DCL[maxResIndex];
+    }
+}
+
+void Analyzer::calcCCL()
+{
+    int16_t currentLimit = 0x7FFF;
+
+    for(uint8_t c = 0; c < NUM_CHIPS; c++)
+    {
+        for(uint8_t cell = 0; cell < NUM_CELLS_PER_CHIP; cell++)
+        {   
+            // Apply equation
+            uint16_t tmpCCL = (MAX_VOLT - ((bmsdata->chipData[c].openCellVoltage[cell] + VOLT_SAG_MARGIN) * 10000)) / (bmsdata->chipData[c].cellResistance[cell] * 10);
+            // Multiplying resistance by 10 to convert from mOhm to Ohm and then to Ohm * 10000 to account for the voltage units
+
+            //Taking the minimum DCL of all the cells
+            if(tmpCCL < currentLimit) currentLimit = tmpCCL;
+        }
+    }
+
+    // ceiling for current limit
+    if (currentLimit > MAX_CELL_CURR) 
+    {
+        bmsdata->dischargeLimit = MAX_CELL_CURR;
+    }
+    else 
+    {
+        bmsdata->dischargeLimit = currentLimit;
     }
 }
 
