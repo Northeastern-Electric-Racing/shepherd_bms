9959bc195293fde84198622dd7177702ab9c2f37
dyldonahue <98500199+dyldonahue@users.noreply.github.com>
2023-03-02 21:43:49 -0500

    techdebt and styling (#148)
    * deleted calcs.h and calcs.cpp
    
    * fixed declaration of calcPWM
    
    * used existing macros for CAN messages
    
    * fixed CANMSG unions to no longer be static
    
    * implemented ROS style naming across entire repo + Nerduino repo
    
    * updated submodule
    
    * implemented ROS style naming across entire repo + Nerduino repo
    
    updated submodule
    
    * fixed thermisterValue thing
    
    * updated naming convention for structs

--- shepherd_bms/include/compute.h
+++ shepherd_bms/include/compute.h
@@ -14,15 +14,14 @@
 #define CHARGER_BAUD          250000U
 #define MC_BAUD               1000000U
 
-#define CHARGER_CAN_ID        0x18E54024
 
 #define MAX_ADC_RESOLUTION    1023 // 13 bit ADC
 
 class ComputeInterface
 {
    private:
-      uint8_t fanSpeed;
-      bool isChargingEnabled;
+      uint8_t fan_speed_;
+      bool is_charging_enabled_;
 
       enum
       {
@@ -48,19 +47,19 @@ class ComputeInterface
       /**
       * @brief sets safeguard bool to check whether charging is enabled or disabled
       *
-      * @param isEnabled
+      * @param is_enabled
       */
-      void enableCharging(bool isEnabled);
+      void enableCharging(bool enable_charging);
 
       /**
       * @brief sends charger message
       * 
-      * @param voltageToSet
+      * @param voltage_to_set
       * @param currentToSet
       *
       * @return Returns a fault if we are not able to communicate with charger
       */
-      FaultStatus_t sendChargingMessage(uint16_t voltageToSet, AccumulatorData_t *bms_data);
+      FaultStatus_t sendChargingMessage(uint16_t voltage_to_set, AccumulatorData_t *bms_data);
 
       /**
       * @brief Returns if we are detecting a charging voltage
@@ -82,9 +81,9 @@ class ComputeInterface
       /**
       * @brief Sets the desired fan speed
       *
-      * @param newFanSpeed
+      * @param new_fan_speed
       */
-      void setFanSpeed(uint8_t newFanSpeed);
+      void setFanSpeed(uint8_t new_fan_speed);
 
       /**
       * @brief Returns the pack current sensor reading
@@ -96,88 +95,88 @@ class ComputeInterface
       /**
       * @brief sends max charge/discharge current to Motor Controller
       *
-      * @param maxCharge
-      * @param maxDischarge
+      * @param max_charge
+      * @param max_discharge
       */
-      void sendMCMsg(uint16_t maxCharge, uint16_t maxDischarge);
+      void sendMCMsg(uint16_t max_charge, uint16_t max_discharge);
 
       /**
       * @brief updates fault relay
       *
-      * @param faultState
+      * @param fault_state
       */
-      void setFault(FaultStatus_t faultState);
+      void setFault(FaultStatus_t fault_state);
 
       /**
       * @brief sends acc status message
       * 
       * @param voltage
       * @param current
-      * @param AH
-      * @param SoC
+      * @param ah
+      * @param soc
       * @param health
       *
       * @return Returns a fault if we are not able to send
       */
-      void sendAccStatusMessage(uint16_t voltage, int16_t current, uint16_t AH, uint8_t SoC, uint8_t health);
+      void sendAccStatusMessage(uint16_t voltage, int16_t current, uint16_t ah, uint8_t soc, uint8_t health);
 
       /**
       * @brief sends BMS status message 
       * 
       * @param failsafe
-      * @param dtc1
-      * @param dtc2
-      * @param currentLimit
-      * @param tempAvg
-      * @param tempInternal
+      * @param dtc_1
+      * @param dtc_2
+      * @param current_limit
+      * @param avg_temp
+      * @param internal_temp
       * 
       * @return Returns a fault if we are not able to send
       */
-      void sendBMSStatusMessage(uint8_t failsafe, uint8_t dtc1, uint16_t dtc2, uint16_t currentLimit, int8_t tempAvg, int8_t tempInternal);
+      void sendBMSStatusMessage(uint8_t failsafe, uint8_t dtc_1, uint16_t dtc_2, uint16_t current_limit, int8_t avg_temp, int8_t internal_temp);
 
       /**
       * @brief sends shutdown control message
       * 
-      * @param mpeState
+      * @param mpe_state
       *
       * @return Returns a fault if we are not able to send
       */
-      void sendShutdownControlMessage(uint8_t mpeState);
+      void sendShutdownControlMessage(uint8_t mpe_state);
 
       /**
       * @brief sends cell data message
       * 
       * @param hv
-      * @param hvID
+      * @param hv_id
       * @param lv
-      * @param lvID
-      * @param voltAvg
+      * @param lv_id
+      * @param avg_volt
       *
       * @return Returns a fault if we are not able to send
       */
-      void sendCellDataMessage(uint16_t hv, uint8_t hvID, uint16_t lv, uint8_t lvID, uint16_t voltAvg);
+      void sendCellDataMessage(uint16_t hv, uint8_t hv_id, uint16_t lv, uint8_t lv_id, uint16_t avg_volt);
 
       /**
       * @brief sends cell voltage message
       * 
-      * @param cellID
-      * @param instantVoltage
-      * @param internalResistance
+      * @param cell_id
+      * @param instant_volt
+      * @param internal_res
       * @param shunted
-      * @param openVoltage
+      * @param open_voltage
       *
       * @return Returns a fault if we are not able to send
       */
-      void sendCellVoltageMessage(uint8_t cellID, uint16_t instantVoltage, uint16_t internalResistance, uint8_t shunted, uint16_t openVoltage);
+      void sendCellVoltageMessage(uint8_t cell_id, uint16_t instant_volt, uint16_t internal_res, uint8_t shunted, uint16_t open_voltage);
 
       /**
       * @brief sends "is charging" message
       * 
-      * @param chargingStatus
+      * @param charging_status
       *
       * @return Returns a fault if we are not able to send
       */
-      void sendChargingStatus(bool chargingStatus);
+      void sendChargingStatus(bool charging_status);
 
       /**
        * @brief sends out the calculated values of currents
